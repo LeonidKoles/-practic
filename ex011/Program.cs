5fba67e78a48cbda92c50b805d614ada61d68225
Колесниченко Леонид <Leon-kol1994@rambler.ru>
2022-07-10 13:48:48 +0300

    dfh

---
+++ ex011/Program.cs
@@ -0,0 +1,44 @@
+﻿    void FillArray(int[] collection)
+    {
+        int length = collection.Length;
+        int index = 0;
+        while (index < length)
+        {
+            collection[index] = new Random().Next(1, 10);     
+            index++;
+        }
+    }
+
+
+    void PrintArray(int[] col)
+    {
+        int count = col.Length;
+        int position = 0;
+        while (position < count)
+        {
+            Console.WriteLine(col[position]);
+            position++;
+        }
+    }
+
+int Index0f(int[] collection, int find)
+{
+    int count = collection.Length;
+    ind index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[] == find)
+        {
+            position
+        }
+        index++;
+    }
+
+
+}
+
+int[] array = new int(10);
+
+FillArray(array);
+PrintArray(array);
